eb59acea65f551bf11c1d46ebd382cb5d45f852f
Nigel Charman <nigel.charman.nz@gmail.com>
2014-10-26 12:46:05 +1300

    Added index files to show that an HTML spec can run Excel specs using the concordion:run command

---
+++ src/test/java/org/concordion/ext/excel/Index.java
@@ -0,0 +1,8 @@
+package org.concordion.ext.excel;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcordionRunner.class)
+public class Index {
+}
