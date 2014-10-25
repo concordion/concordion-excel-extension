f1c2fc0ae6017dbcf26ef470c8c05c2f4796e247
Nigel Charman <nigel.charman.nz@gmail.com>
2014-10-25 21:09:49 +1300

    Fixed issue with HTML entities, such as &pound; being referenced, but not declared

--- src/test/java/org/concordion/ext/excel/CellConversions.java
+++ src/test/java/org/concordion/ext/excel/CellConversions.java
@@ -12,6 +12,9 @@ public class CellConversions extends AbstractSpecTest {
 	public String getBody() {
 		return "<p><span>Number Examples</span> <span>1.50</span> <span>3</span> <span>6,100.105</span> <span>1.7</span></p>"
 				+ "<p><span>Date Examples</span> <span>03 July 2014</span> <span>15/01/2021</span> <span>3-Mar</span> <span>9-Apr-13</span> <span>1/3/1806</span></p>"
-				+ "<p><span>Function</span> <span>6</span> <span>1/19/21</span> <span>AB</span></p><p><span>Boolean</span> <span>true</span> <span>false</span></p>";
+				+ "<p><span>Function</span> <span>6</span> <span>1/19/21</span> <span>AB</span></p>"
+				+ "<p><span>Boolean</span> <span>true</span> <span>false</span></p>"
+				+ "<p><span>Currency</span> <span>$6.79</span> <span>£50.00</span> <span>฿1,234.56</span></p>"
+				+ "<p><span>Text with XML entities</span> <span>&lt; &amp; &gt; &apos; &quot;</span></p>";
 	}
 }
