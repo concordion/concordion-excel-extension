9a77b82df681ed18559dea727b0d229a2fa85299
Vlad Gheorghe <vlad.gheorghe@gmail.com>
2019-09-17 22:01:00 +0300

    add cell location information in exception handling where possible

--- src/main/java/org/concordion/ext/excel/ExcelCellConversionException.java
+++ src/main/java/org/concordion/ext/excel/ExcelCellConversionException.java
@@ -1,13 +1,26 @@
 package org.concordion.ext.excel;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.util.CellReference;
 
 public class ExcelCellConversionException extends ExcelConversionException {
 
 	private static final long serialVersionUID = -8957983421526078295L;
 
 	public ExcelCellConversionException(String arg0, Cell c) {
-		super(arg0+" at "+c.getRowIndex()+"x"+c.getColumnIndex());
+		super(arg0+" at cell "+c.getAddress());
+	}
+	
+	public ExcelCellConversionException(String arg0, Cell c, Throwable t) {
+		super(arg0+" at cell "+c.getAddress(), t);
+	}
+
+	public ExcelCellConversionException(String arg0, CellReference c) {
+		super(arg0+" at cell "+c.formatAsString());
+	}
+
+	public ExcelCellConversionException(String arg0, CellReference c, Throwable t) {
+		super(arg0+" at cell "+c.formatAsString(), t);
 	}
 
 }
