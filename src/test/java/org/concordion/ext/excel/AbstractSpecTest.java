bed9540cec5aafffd106d28b773f4f7b999cc7f5
Vlad Gheorghe <vlad.gheorghe@gmail.com>
2019-09-17 22:28:46 +0300

    tests ignore trace attribute

--- src/test/java/org/concordion/ext/excel/AbstractSpecTest.java
+++ src/test/java/org/concordion/ext/excel/AbstractSpecTest.java
@@ -6,13 +6,16 @@ import java.util.Map;
 
 import javax.xml.transform.Source;
 
+import org.concordion.ext.excel.conversion.AbstractConversionStrategy;
 import org.junit.Assert;
+import org.w3c.dom.Attr;
 import org.xmlunit.builder.Input;
 import org.xmlunit.diff.Comparison;
 import org.xmlunit.diff.ComparisonListener;
 import org.xmlunit.diff.ComparisonResult;
 import org.xmlunit.diff.DOMDifferenceEngine;
 import org.xmlunit.diff.DifferenceEngine;
+import org.xmlunit.util.Predicate;
 
 
 /**
@@ -50,6 +53,13 @@ public abstract class AbstractSpecTest {
                 Assert.fail("found a difference: " + comparison);
             }
         });
+        diff.setAttributeFilter(new Predicate<Attr>() {
+        	public boolean test(Attr attr) {
+        		// ignore traceability attributes
+        		return ! attr.getName().equals(AbstractConversionStrategy.ATTR_TRACE);
+        	}
+		});
+
 
         diff.compare(expected, actual);
 
